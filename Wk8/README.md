525f68866b692e821bd8e4717e30e73486989eb8
Ryan Romero <raromero103@gmail.com>
2024-03-18 01:15:20 -0700

    added starter code. Updated README

---
+++ Wk8/README.md
@@ -0,0 +1,32 @@
+# CS 11 SI Week 7/8: Vectors & Functions
+
+## What we will do:
+Create a simple program that uses vectors and provides different operations for them.
+
+## Instructions:
+Download this repository by clicking the green code button, and then download as a zip. Extract this zip folder on to your desktop and open it is VSC.
+
+Complete the code by writing the functions based on their given function definitions.
+
+## Initial Questions:
+* What are the lines of code above our main function called? What is their purpose?
+* Would our program sitll work without them?
+* What is each function returning?
+
+## Steps
+1. Complete each function by implementing their definition.
+
+Sample Output:
+```
+Enter a vector size: 5
+Enter 5 integers, separated by a space:
+4 6 9 1 2
+
+Original Vector: 4 6 9 1 2 
+Reversed: 2 1 9 6 4 
+Sum of elements: 22
+```
+
+## Review Quetions:
+* Why do we have the reverse function return a new vector instead of just printing the original bakwards?
+* Are there alternative ways to perform sum and reverse?
---
+++ Wk8/vectorFunctions.cpp
@@ -0,0 +1,61 @@
+/*
+    CS-11 SI Vector & Function practice, vectorFunctions.cpp
+    Purpose: Practice looping through a string and using an if statement
+
+    @author Your Name
+    @version 1.0  00/00/2024
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Prints a vector.
+ *
+ * @param v The vector to be printed
+ */
+void printVector(vector<int> v);
+
+/**
+ * Calculates the sum of a vector.
+ *
+ * @param v The vector containing numbers to be summed
+ * @return The sum of the elements
+ */
+int sumVector(vector<int> v);
+
+/**
+ * Reverses a vector.
+ *
+ * @param v The vector containing numbers to be reversed
+ * @return A new vector of identical size with the elements reversed
+ */
+vector<int> reverserVector(vector<int> v);
+
+int main() {
+    // Have the user fill a vector of ints
+    int size, num;
+    vector<int> myVector;
+
+    cout << "Enter a vector size: ";
+    cin >> size;
+    cout << "Enter " << size << " integers, separated by a space:\n";
+    for (int i = 0; i < size; i++) {
+        cin >> num;
+        myVector.push_back(num);
+    }
+
+    // Print different calculations of the vector
+    vector<int> myVectorReversed = reverserVector(myVector);
+    int sum = sumVector(myVector);
+
+    cout << "\nOriginal Vector: ";
+    printVector(myVector);
+    cout << "Reversed: ";
+    printVector(myVectorReversed);
+    cout << "Sum of elements: " << sum << endl;
+
+    return 0;
+}
